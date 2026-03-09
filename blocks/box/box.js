dcfe17e8f6aa7f8e3ab380aa7db20b1c7f164315
yesalpiz <yesalpiz@publicisgroupe.net>
2026-03-09 11:16:57 -0600

    Fix wrapper styling - test

--- blocks/box/box.js
+++ blocks/box/box.js
@@ -3,13 +3,17 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 export default function decorate(block) {
   /* change to ul, li */
   const divParent = document.createElement('div');
+  divParent.className = 'box-parent';
   [...block.children].forEach((row) => {
     const divchildren = document.createElement('div');
-    while (row.firstElementChild) divchildren.append(row.firstElementChild);
+    while (row.firstElementChild) {
+      divchildren.append(row.firstElementChild);
+    }
     [...divchildren.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'box-content-image';
       else div.className = 'box-content-body';
     });
+    divchildren.className = 'box-content-wrapper';
     divParent.append(divchildren);
   });
   divParent.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
